9b6938cd74ddc8f885a4cf13a4b16362bebfe20f
Vinay Keerthi <Vinay.Keerthi@gkndriveline.com>
2018-07-30 17:34:24 +0530

    Changed version to 0.1.1a

--- setup.py
+++ setup.py
@@ -5,12 +5,23 @@ with open("README.md", "r") as f:
 
 setup(
     name="svnlib",
-    version="0.1a",
+    version="0.1.1a",
     author="Vinay Keerthi",
     author_email="ktvkvinaykeerthi@gmail.com",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    packages=find_packages(),
+    packages=find_packages(
+        exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    url="https://vinay87.github.io/svnlib",
+    license="MIT",
+    tests_require=["pytest"],
+    
+    project_urls={
+        "Documentation": "https://vinay87.github.io/svnlib", # Replace with readthedocs.
+        "Source Code": "https://github.com/vinay87/svnlib",
+        "Bug Tracker": "https://github.com/vinay87/svnlib/issues"
+        },
+
     classifiers=(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 2.7",
