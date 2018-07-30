56ff19cb1f1891266e122127666974f2d80ab22e
Vinay Keerthi <Vinay.Keerthi@gkndriveline.com>
2018-07-30 14:24:15 +0530

    Added additional metadata to the setup file.

--- setup.py
+++ setup.py
@@ -1,7 +1,23 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as f:
+    long_description = f.read()
+
 setup(
     name="svnlib",
     version="0.1dev",
+    author="Vinay Keerthi",
+    author_email="ktvkvinaykeerthi@gmail.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 2 - Pre-Alpha",
+        "Topic :: Software Development :: Version Control",
+        "Intended Audience :: Developers"
+    )
 )
